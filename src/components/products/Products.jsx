04130ea70e81f730721bdce0ff699c6725527469
Shamsi <zaheb.shamsi@gmail.com>
2019-05-18 20:12:41 +0530

    card added on products

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,45 +1,62 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
+import { Card, CardHeader, CardMedia, CardContent, CardActions, Avatar, IconButton, Typography, Button } from '@material-ui/core';
+import { FavoriteIcon, ShareIcon, MoreVertIcon } from '@material-ui/icons';
 
 
-export default class Products extends Component{
-    constructor(props){
+export default class Products extends Component {
+    constructor(props) {
         super(props)
         this.state = {
-            cartAddAlert : false
+            cartAddAlert: false
         }
-        this.onClickHandler =this.onClickHandler.bind(this)
+        this.onClickHandler = this.onClickHandler.bind(this)
     }
     onClickHandler() {
         this.props.products.forEach(element => {
-            if(element.quantity>0){
+            if (element.quantity > 0) {
                 this.setState({
-                    cartAddAlert :true
+                    cartAddAlert: true
                 })
             }
         });
         this.props.addToCart();
     }
 
-    render(){
-        return(
-            <div> 
-                <table>        
-                    {this.props.products.map((product)=>(
-                                            <tr>
-                                                <td>{product.name}</td>
-                                                <td>{product.price}</td>
-                                                <td>{product.quantity}</td>                                             
-                                                <button onClick={() => this.props.onIncrementQuantity(product.name)}>+</button>                                          
-                                                <button onClick={() => {
-                                                    if(product.quantity >0){
-                                                        this.props.onDecrementQuantity(product.name)}
-                                                        }}>-</button>
-                                            </tr>
-                    ))}              
-                </table> 
-                <button onClick = {this.onClickHandler}>Add to Cart</button>  
-                {this.state.cartAddAlert && <label>Product added to cart.Visit <a href="./#/dashboard">Dashboard</a> to view your cart. </label> }  
-                {/* <ul>{this.props.serverData.map((products) => <li>{products.name}</li>)}</ul> */}
+    render() {
+        return (
+            <div>
+                <div>
+                    <table>
+                        {this.props.products.map((product) => (
+                            <tr>
+                                <td>{product.name}</td>
+                                <td>{product.price}</td>
+                                <td>{product.quantity}</td>
+                                <button onClick={() => this.props.onIncrementQuantity(product.name)}>+</button>
+                                <button onClick={() => {
+                                    if (product.quantity > 0) {
+                                        this.props.onDecrementQuantity(product.name)
+                                    }
+                                }}>-</button>
+                            </tr>
+                        ))}
+                    </table>
+                    <button onClick={this.onClickHandler}>Add to Cart</button>
+                    {this.state.cartAddAlert && <label>Product added to cart.Visit <a href="./#/dashboard">Dashboard</a> to view your cart. </label>}
+                </div>
+                <div>
+                    {this.props.products.map((product) => (
+                        <div>
+                            <Card>
+                            <CardHeader
+                                title= {product.name}
+                                subheader={ 'Rs' + product.price} />
+                            <Button variant="contained" color="primary">+</Button>
+                            <Button variant="contained" color="primary">-</Button>
+                            </Card>
+                        </div>
+                    ))}
+                </div>
             </div>
         )
     }
